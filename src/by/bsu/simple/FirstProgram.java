afcfe58b2b9a55f3940ed549ac00aa814c9c7383
mikhail-peredirey <com.epam.mikhail-peredirey>
2015-05-25 17:00:07 +0600

    Initial commit

---
+++ src/by/bsu/simple/FirstProgram.java
@@ -0,0 +1,10 @@
+package by.bsu.simple;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        //oбъявление и создание объекта firstObject
+        SloganAction firstObject = new SloganAction();
+        //вызов метода, содержащего вывод строки
+        firstObject.printSlogan();
+    }
+}
---
+++ src/by/bsu/simple/SloganAction.java
@@ -0,0 +1,8 @@
+package by.bsu.simple;
+
+public class SloganAction {
+    void printSlogan() { //определение метода
+        //вывод строки
+        System.out.println("Enabling the Information Age");
+    }
+}
